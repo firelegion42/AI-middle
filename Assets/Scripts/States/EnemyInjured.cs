a5b93ccbc5d46af400a51a829b8eb6b966d20e3c
firelegion42 <firelegion9706@gmail.com>
2026-09-07 18:20:28 +0300

    Cleaner Version of first logic

--- Assets/Scripts/States/EnemyInjured.cs
+++ Assets/Scripts/States/EnemyInjured.cs
@@ -5,15 +5,17 @@ public class EnemyInjuredState : EnemyState
     public override void EnterState(EnemyStateManager state)
     {
         moveSpeedCoef = 0.5f;
+
+        Debug.Log("EnteredInjured");
     }
 
     public override void OnHealthChanged(EnemyStateManager state, float health)
     {
-        throw new System.NotImplementedException();
+        
     }
 
     public override void UpdateState(EnemyStateManager state)
     {
-        throw new System.NotImplementedException();
+        
     }
 }
